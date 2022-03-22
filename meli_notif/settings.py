2af9478c434d32885c03cfbf2280aa4c7ceee6f6
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2022-03-21 23:36:42 -0300

    allowed cors for front

--- meli_notif/settings.py
+++ meli_notif/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = (
   'https://juanpicasti.github.io/meli-notif-front/',
 )
