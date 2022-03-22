6bcc4fe816e840f84bb44b51c04b8428c154fbe3
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2022-03-21 21:49:09 -0300

    changed db to postgres

--- meli_notif/settings.py
+++ meli_notif/settings.py
@@ -79,6 +79,9 @@ WSGI_APPLICATION = 'meli_notif.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DATABASE_URL'),
     }
 }
